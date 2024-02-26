c0cfe10335ba61c7366c44fb0491a04e45c2ec1b
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-02-26 16:38:43 +0100

    Login with empty fields test

---
+++ src/main/java/org/example/testCases/LoginLogout/LoginEmptyFields.java
@@ -0,0 +1,28 @@
+package org.example.testCases.LoginLogout;
+
+import org.example.LogIn;
+import org.example.testCases.Runnable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginEmptyFields implements Runnable {
+   private final WebDriver webDriver;
+
+    private static final String URL = "https://jira-auto.codecool.metastage.net/login.jsp?os_destination=%2Fsecure%2FRapidBoard.jspa%3FrapidView%3D3%26projectKey%3DMTP";
+
+
+    public LoginEmptyFields(WebDriver webDriver) {
+        this.webDriver = webDriver;
+    }
+
+    @Override
+    public void run() {
+        webDriver.navigate().to(URL);
+        webDriver.navigate().to(URL);
+
+        WebElement button = webDriver.findElement(By.id("login-form-submit"));
+
+        button.click();
+    }
+}
