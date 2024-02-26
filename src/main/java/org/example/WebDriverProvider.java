a76cb750b2bf9681f5ee8735f273fd0b07cd32ec
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-02-26 13:34:26 +0100

    Refactor Main to WebDriverProvider

--- src/main/java/org/example/WebDriverProvider.java
+++ src/main/java/org/example/WebDriverProvider.java
@@ -5,14 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class Main {
+public class WebDriverProvider {
 
     public static final String BROWSER_VERSION = "121";
-
-    public static void main(String[] args) {
-        setupWebDriver();
-
-    }
 
     public static WebDriver setupWebDriver() {
         WebDriverManager.chromedriver().setup();
