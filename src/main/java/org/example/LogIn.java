11ea120a0f526f53df914856ac4cd1b0b0ff0e88
Mark Veszelka <120094709+markveszelka@users.noreply.github.com>
2024-02-26 19:55:36 +0100

    Introduce environment variables for jira username and password

--- src/main/java/org/example/LogIn.java
+++ src/main/java/org/example/LogIn.java
@@ -1,12 +1,13 @@
 package org.example;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class LogIn {
-    public static final String USER_NAME = "automation74";
-    public static final String USER_PASSWORD = "CCAutoTest19.";
+
+    private final Dotenv dotenv = Dotenv.load();
     private static final String URL = "https://jira-auto.codecool.metastage.net/login.jsp?os_destination=%2Fsecure%2FRapidBoard.jspa%3FrapidView%3D3%26projectKey%3DMTP";
     private final WebDriver webDriver;
 
@@ -20,8 +21,8 @@ public class LogIn {
         WebElement userPassword = webDriver.findElement(By.id("login-form-password"));
         WebElement button = webDriver.findElement(By.id("login-form-submit"));
 
-        userName.sendKeys(USER_NAME);
-        userPassword.sendKeys(USER_PASSWORD);
+        userName.sendKeys(dotenv.get("JIRA_USERNAME"));
+        userPassword.sendKeys(dotenv.get("JIRA_PASSWORD"));
         button.click();
     }
 }
