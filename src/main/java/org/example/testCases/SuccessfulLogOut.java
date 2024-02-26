17edea14c08f0e6cae24752809cffaabfb563d5a
Sarfeher <132908720+Sarfeher@users.noreply.github.com>
2024-02-26 14:49:53 +0100

    Test log out

---
+++ src/main/java/org/example/testCases/SuccessfulLogOut.java
@@ -0,0 +1,30 @@
+package org.example.testCases;
+
+import org.example.LogIn;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class SuccessfulLogOut implements Runnable {
+    WebDriver webDriver;
+    LogIn logIn;
+
+    private final String URL = "https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa";
+
+    public SuccessfulLogOut(WebDriver webDriver, LogIn logIn) {
+        this.webDriver = webDriver;
+        this.logIn = logIn;
+    }
+
+    @Override
+    public void run() {
+        logIn.logIn();
+        webDriver.navigate().to(URL);
+        WebElement dropDown = webDriver.findElement(By.id("header-details-user-fullname"));
+        dropDown.click();
+
+        WebElement logOutButton = webDriver.findElement(By.id("log_out"));
+        logOutButton.click();
+
+    }
+}
---
+++ src/test/java/org/example/testCases/SuccessfulLogOutTest.java
@@ -0,0 +1,40 @@
+package org.example.testCases;
+
+import org.example.LogIn;
+import org.example.WebDriverProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuccessfulLogOutTest {
+
+    private WebDriver webDriver;
+    private LogIn logIn;
+
+    @BeforeEach
+    public void setup(){
+        webDriver = WebDriverProvider.setupWebDriver();
+        logIn = new LogIn(webDriver);
+    };
+
+    @Test
+    public void test(){
+        SuccessfulLogOut successfulLogOut = new SuccessfulLogOut(webDriver, logIn);
+        successfulLogOut.run();
+
+        WebElement userOptions = webDriver.findElement(By.id("user-options"));
+        Assertions.assertNotNull(userOptions);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        webDriver.quit();
+    }
+
+}
